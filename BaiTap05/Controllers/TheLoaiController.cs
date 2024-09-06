d906c1f388e727b6afe172783f49955b5ac8332c
tmihhai04 <tranminhhai120104@example.com>
2024-09-07 00:55:26 +0700

    hoan thanh bai tap

---
+++ BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,25 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngay 1";
+            ViewBag.Thang = "Thang 1";
+            ViewData["Nam"] = "Nam 2024";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh tham"
+            };
+            return View(theloai);
+        }
+    }
+}

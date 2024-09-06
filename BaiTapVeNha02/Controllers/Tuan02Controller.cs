949596daac5065e8c46db4269477a499a91b3a05
tmihhai04 <tranminhhai120104@example.com>
2024-09-07 01:11:02 +0700

    hoan thanh bai tap

---
+++ BaiTapVeNha02/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha02.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Tran Minh Hai";
+            ViewBag.MSSV = "1822041114";
+            ViewBag.Nam = "Nam 2024";
+            return View();
+        }
+    }
+}

46080fb078ec387c4e6a9f2b47444085b5c9abc6
tmihhai04 <tranminhhai120104@example.com>
2024-09-07 08:27:23 +0700

    hoan thanh bai tap

---
+++ BaiTapVeNha03/Controllers/Tuan2Controller.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha03.Controllers
+{
+    public class Tuan2Controller : Controller
+    {
+        public IActionResult MayTinh(double a, double b, string pheptinh)
+        {
+            double ketqua = 0;
+            string error = null;
+
+            // Xử lý phép tính dựa trên giá trị của pheptinh
+            switch (pheptinh)
+            {
+                case "cong":
+                    ketqua = a + b;
+                    break;
+                case "tru":
+                    ketqua = a - b;
+                    break;
+                case "nhan":
+                    ketqua = a * b;
+                    break;
+                case "chia":
+                    if (b != 0)
+                    {
+                        ketqua = a / b;
+                    }
+                    else
+                    {
+                        error = "Không thể chia cho 0";
+                    }
+                    break;
+                default:
+                    error = "Phép tính không hợp lệ";
+                    break;
+            }
+
+            // Lưu kết quả vào ViewBag
+            if (error == null)
+            {
+                ViewBag.KetQua = ketqua;
+            }
+            else
+            {
+                ViewBag.Error = error;
+            }
+            return View();
+        }
+    }
+}
---
+++ BaiTapVeNha03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaiTapVeNha03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

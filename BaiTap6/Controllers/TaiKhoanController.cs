8d24e232d17006433107d4e0491368b53add2959
tmihhai04 <tranminhhai120104@example.com>
2024-09-14 03:09:23 +0700

    hoan thanh bai tap

---
+++ BaiTap6/Controllers/TaiKhoanController.cs
@@ -0,0 +1,21 @@
+﻿using BaiTap6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap6.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taikhoan)
+        {
+            if (taikhoan != null && taikhoan.Password != null && taikhoan.Password.Length > 0)
+            {
+                taikhoan.Password = taikhoan.Password + "_da_ma_hoa";
+            }
+            return View(taikhoan);
+        }
+    }
+}
---
+++ BaiTap6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaiTap6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

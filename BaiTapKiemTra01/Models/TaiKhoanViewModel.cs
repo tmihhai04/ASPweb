ab7fb9fbc1bbcb31fd53a164afdbca742c5307da
tmihhai04 <tranminhhai120104@example.com>
2024-09-14 15:16:06 +0700

    hoan thanh bai tap

---
+++ BaiTapKiemTra01/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BaiTapKiemTra01.Models
+{
+    public class TaiKhoanViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}

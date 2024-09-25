0ef5782437d6f36ec8645f577cdff0fa9f58d15f
tmihhai04 <tranminhhai120104@example.com>
2024-09-25 12:50:59 +0700

    hoan thanh bai tap

---
+++ BaiTap07/Controllers/TheLoaiController.cs
@@ -0,0 +1,90 @@
+﻿using BaiTap07.Data;
+using BaiTap07.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap07.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TheLoaiController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var theloai = _db.TheLoai.ToList();
+            ViewBag.TheLoai = theloai;
+            return View();
+        }
+
+        [HttpGet]        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TheLoai theloai)
+        {
+            if (ModelState.IsValid)
+            {     
+            //Thêm thông tin
+            _db.TheLoai.Add(theloai);
+            //Lưu
+            _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.TheLoai.Find(id);
+            return View(theloai);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TheLoai theloai)
+        {
+            if (ModelState.IsValid)
+            {
+                //Thêm thông tin
+                _db.TheLoai.Update(theloai);
+                //Lưu
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.TheLoai.Find(id);
+            return View(theloai);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var theloai = _db.TheLoai.Find(id);
+            if (theloai == null)
+            {
+                return NotFound();
+            }
+            _db.TheLoai.Remove(theloai);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BaiTap07/Models/TheLoai.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiTap07.Models
+{
+    public class TheLoai
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Không được để trống tên thể loại!")]
+        [Display(Name = "Thể loại")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Không đúng định dạng ngày!")]
+        [Display(Name="Ngày tạo")]
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}

909fbf41c9699d7dc32fc72c5e03766b1fcaf813
tmihhai04 <tranminhhai120104@example.com>
2024-09-28 15:39:11 +0700

    hoan thanh abi tap

---
+++ BaiKiemTra02/BaiKiemTra02/Controllers/LopHocController.cs
@@ -0,0 +1,105 @@
+﻿using BaiKiemTra02.Data;
+using BaiKiemTra02.Data.Migrations;
+using BaiKiemTra02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra02.Controllers
+{
+    public class LopHocController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LopHocController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var lophoc = _db.LopHoc.ToList();
+            ViewBag.lophoc = lophoc;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(LopHoc lopHoc)
+        {
+            if (ModelState.IsValid)
+            {
+                //them thong tin
+                _db.LopHoc.Add(lopHoc);
+                //luu lai thong tin
+                _db.SaveChanges();
+                //chuyen ve trang index
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var lophoc = _db.LopHoc.Find(id);
+            return View(lophoc);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(LopHoc lopHoc)
+        {
+            if (ModelState.IsValid)
+            {
+                //them thong tin
+                _db.LopHoc.Update(lopHoc);
+                //luu lai thong tin
+                _db.SaveChanges();
+                //chuyen ve trang index
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var lophoc = _db.LopHoc.Find(id);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var lophoc = _db.LopHoc.Find(id);
+            if (lophoc == null)
+            {
+                return NotFound();
+            }
+            _db.LopHoc.Remove(lophoc);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var lophoc = _db.LopHoc.Find(id);
+            return View(lophoc);
+        }
+    }
+}
